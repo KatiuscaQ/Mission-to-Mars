7c9e6a6dd4a19d704000222c1abc2ace73b2f543
Katiusca <quijadakatiusca@gmail.com>
2020-12-07 23:53:44 -0500

    Deliverable 3: Update some cool stuff

--- scraping.py
+++ scraping.py
@@ -100,7 +100,7 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-striped")
+    return df.to_html(classes="table table-hover")
 
 def mars_hemispheres(browser):
     # 1. Use browser to visit the URL 
